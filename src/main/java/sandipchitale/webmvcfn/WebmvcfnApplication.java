732090aca01092146aac230076ba5242454a0a0d
SANDIP CHITALE <sandipchitale@gmail.com>
2023-11-13 19:47:27 -0800

    Initial commit.

---
+++ src/main/java/sandipchitale/webmvcfn/WebmvcfnApplication.java
@@ -0,0 +1,58 @@
+package sandipchitale.webmvcfn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.MediaType;
+import org.springframework.web.client.RestTemplate;
+import org.springframework.web.servlet.function.EntityResponse;
+import org.springframework.web.servlet.function.RouterFunction;
+import org.springframework.web.servlet.function.ServerRequest;
+import org.springframework.web.servlet.function.ServerResponse;
+
+import static org.springframework.web.servlet.function.RequestPredicates.methods;
+import static org.springframework.web.servlet.function.RequestPredicates.path;
+import static org.springframework.web.servlet.function.RouterFunctions.route;
+
+@SpringBootApplication
+public class WebmvcfnApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(WebmvcfnApplication.class, args);
+	}
+
+	private static record Person(String name, int age) {}
+
+	@Bean
+	RouterFunction<ServerResponse> routes(RestTemplateBuilder restTemplateBuilder) {
+		String X_METHOD = "X-METHOD";
+		RestTemplate restTemplate = restTemplateBuilder.rootUri("https://postman-echo.com/").build();
+		return route()
+				.before((ServerRequest request) -> {
+					return ServerRequest.from(request).header(X_METHOD, request.method().name().toLowerCase()).build();
+				})
+				.route(path("/").and(methods(HttpMethod.GET, HttpMethod.POST, HttpMethod.PUT, HttpMethod.DELETE)), (ServerRequest request) -> {
+					HttpEntity<Person> personEntity = new HttpEntity<>(new Person("somebody", 42));
+					return ServerResponse.ok().body(
+							restTemplate.exchange("/" + request.method().name().toLowerCase(),
+									request.method(),
+									personEntity,
+									String.class));
+				})
+				.after((ServerRequest request, ServerResponse response) -> {
+					if (response instanceof EntityResponse<?> entityResponse) {
+						return ServerResponse
+								.from(entityResponse)
+								.header(X_METHOD, request.method().name().toLowerCase())
+								.header(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE)
+								.body(entityResponse.entity());
+					}
+					return response;
+				})
+				.build();
+	}
+
+}
---
+++ src/test/java/sandipchitale/webmvcfn/WebmvcfnApplicationTests.java
@@ -0,0 +1,13 @@
+package sandipchitale.webmvcfn;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebmvcfnApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
